1878321b5f1f8c7efd1706c764cd0826e6bcc401
tendsT0Zero <nayeemofficial80@gmail.com>
2023-09-29 11:56:25 +0600

    new comment added

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
+using namespace std;
 
 // Function to partition the array into two sub-arrays.
 // Elements smaller than the pivot will be on the left,
 // and elements greater than the pivot will be on the right.
-int partition(std::vector<int>& arr, int low, int high) {
+int partition(vector<int>& arr, int low, int high) {
     int pivot = arr[high]; // Choose the last element as the pivot
     int i = (low - 1); // Index of the smaller element
 
     for (int j = low; j <= high - 1; j++) {
         // If the current element is smaller than or equal to the pivot
-        if (arr[j] <= pivot) {
+        if (arr[j] >= pivot) { // increasing / decreasing matter here
             i++; // Increment index of the smaller element
-            std::swap(arr[i], arr[j]);
+            swap(arr[i], arr[j]);
         }
     }
 
-    std::swap(arr[i + 1], arr[high]);
+    swap(arr[i + 1], arr[high]);
     return (i + 1);
 }
 
 // Function to perform quick sort
-void quickSort(std::vector<int>& arr, int low, int high) {
+void quickSort(vector<int>& arr, int low, int high) {
     if (low < high) {
         // Partition the array into two sub-arrays
         int pi = partition(arr, low, high);
@@ -33,22 +33,22 @@ void quickSort(std::vector<int>& arr, int low, int high) {
 }
 
 int main() {
-    std::vector<int> arr = {12, 11, 13, 5, 6, 7};
+    vector<int> arr = {12, 11, 13, 5, 6, 7};
     int n = arr.size();
 
-    std::cout << "Original array: ";
+    cout << "Original array: ";
     for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
+        cout << arr[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
     quickSort(arr, 0, n - 1);
 
-    std::cout << "Sorted array: ";
+    cout << "Sorted array: ";
     for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
+        cout << arr[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 }
